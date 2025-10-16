2ea20d089e8cddc666bf69d069b1ca0673538b01
0323080256-lang <0323080256@htu.edu.gh>
2025-10-16 04:24:05 -0700

    Add files via upload

---
+++ HelloAI_CLI.py
@@ -0,0 +1,16 @@
+# Build: 'Hello AI' CLI that asks your name + print greeting. 
+def HelloAI():
+    YourName = input('Hello AI here, What is your name ? ' + '\n')
+    greeting = 'Welcome'
+    print(f'{greeting}, {YourName}')
+
+HelloAI()
+    
+# End of code.
+
+
+
+
+
+
+
